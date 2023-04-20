149f6bb550c8494a25f5e1cb9ae31be8076ee86d
shaddydevops <122651230+shaddydevops@users.noreply.github.com>
2023-04-20 12:33:08 +0100

    setting up  folder structure for the project

---
+++ src/Data.js
@@ -0,0 +1,53 @@
+
+const Data = [
+      {id:'1',
+      title:'Men Shirt',
+      body:'Lorem ipsum dolor sit amet consectetur. Eget orci viverra vel',
+      price:'29.00',
+      imgUrl:'./assets/menshirt.png' // Update path to assets folder
+      },
+      {id:'2',
+      title:'Nike Sneaker',
+      body:'Lorem ipsum dolor sit amet consectetur. Eget orci viverra vel',
+      price:'39.00',
+      imgUrl:'./assets/nikesneaker.png'
+     },
+     {id:'3',
+     title:'Puma Sneaker',
+     body:'Lorem ipsum dolor sit amet consectetur. Eget orci viverra vel',
+     price:'35.00',
+     imgUrl:'./assets/pumasneaker.png'
+    },
+    {id:'4',
+     title:'Cap',
+     body:'Lorem ipsum dolor sit amet consectetur. Eget orci viverra vel',
+     price:'15.00',
+     imgUrl:'./assets/cap.png'
+    },
+    {id:'5',
+    title:'watch',
+    body:'Lorem ipsum dolor sit amet consectetur. Eget orci viverra vel',
+    price:'50.00',
+    imgUrl:'./assets/watch.png'
+   },
+   {id:'6',
+     title:'Jeans',
+     body:'Lorem ipsum dolor sit amet consectetur. Eget orci viverra vel',
+     price:'26.00',
+     imgUrl:'./assets/jeans.png'
+    },
+    {id:'7',
+     title:'Long Sleeves',
+     body:'Lorem ipsum dolor sit amet consectetur. Eget orci viverra vel',
+     price:'25.00',
+     imgUrl:'./assets/longsleeves.png'
+    },
+    {id:'8',
+    title:'Alonzi Dress',
+    body:'Lorem ipsum dolor sit amet consectetur. Eget orci viverra vel',
+    price:'43.00',
+    imgUrl:'./assets/alonzidress.png'
+   },
+  ]
+  
+  export default Data;
